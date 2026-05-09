122e65d338c321524f9729806f6b4a96e71cebcf
Muhammad Bagus Indrawan <100072786+lithiaa@users.noreply.github.com>
2026-05-09 14:39:16 +0700

    feat: add toaster

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
   import type { Metadata } from "next";
   import { Geist, Geist_Mono } from "next/font/google";
-
+  import { Toaster } from "@/components/ui/sonner"
   import { TooltipProvider } from "@/components/ui/tooltip";
   import "./globals.css";
 
@@ -31,6 +31,7 @@
       >
         <body className="min-h-full flex flex-col">
           <TooltipProvider>{children}</TooltipProvider>
+          <Toaster richColors />
         </body>
       </html>
     );
